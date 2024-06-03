45847899ed69aeb9904f715b3921741d4bdf3e95
liudonghua123 <liudonghua123@gmail.com>
2024-06-03 15:52:14 +0800

    update code

--- dont.js
+++ dont.js
@@ -1,11 +1,15 @@
-var s = document.createElement('script')
-s.textContent =
-  '(function() {' +
-  'var a = Node.prototype.addEventListener;' +
-  'Node.prototype.addEventListener = function(e) {' +
-  "if (e !== 'visibilitychange' && e !== 'webkitvisibilitychange') {" +
-  'a.apply(this, arguments)' +
-  '}}' +
-  '})()'
-;(document.head || document.documentElement).appendChild(s)
-s.remove()
+const s = document.createElement('script');
+s.textContent = `
+  (function() {
+    const disabledEvents = ['visibilitychange', 'webkitvisibilitychange'];
+    Object.defineProperty(document, 'hidden', { value: false, writable: false, });
+    const a = Node.prototype.addEventListener;
+    Node.prototype.addEventListener = function(e) {
+      if (!disabledEvents.includes(e)) {
+        a.apply(this, arguments)
+      }
+    }
+  })();
+`;
+(document.head || document.documentElement).appendChild(s);
+s.remove();

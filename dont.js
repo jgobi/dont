426c2ca8771684b66105d77b201cb8a8c27d6ab5
João Gabriel <joaogabrieloliveira9@gmail.com>
2025-07-15 20:08:17 -0300

    Change script execution method

--- dont.js
+++ dont.js
@@ -1,15 +1,8 @@
-const s = document.createElement('script');
-s.textContent = `
-  (function() {
-    const disabledEvents = ['visibilitychange', 'webkitvisibilitychange'];
-    Object.defineProperty(document, 'hidden', { value: false, writable: false, });
-    const a = Node.prototype.addEventListener;
-    Node.prototype.addEventListener = function(e) {
-      if (!disabledEvents.includes(e)) {
-        a.apply(this, arguments)
-      }
-    }
-  })();
-`;
-(document.head || document.documentElement).appendChild(s);
-s.remove();
+const disabledEvents = ['visibilitychange', 'webkitvisibilitychange'];
+Object.defineProperty(document, 'hidden', { value: false, writable: false, });
+const a = Node.prototype.addEventListener;
+Node.prototype.addEventListener = function(e) {
+  if (!disabledEvents.includes(e)) {
+    a.apply(this, arguments)
+  }
+}
